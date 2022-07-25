22127962fbaed7b5b8bc9f5df2f8f67a295782eb
Will <103394466+OhDangBro@users.noreply.github.com>
2022-07-24 21:51:59 -0400

    Plz commit

--- index.js
+++ index.js
@@ -1,54 +1,138 @@
 const inquirer = require('inquirer');
-console.log("readme working");
+const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const mysql = require('mysql2');
+const app = express();
+app.use(express.json());
 var profileDataArgs = process.argv.slice(10, process.argv.length);
 var commandLineArgs = process.argv;
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Jessie17',
+    database: 'company',
+    multipleStatements: true
+  },
+
+  console.log('Connected to the election database.')
+);
+
+
 function startProgram() {
-inquirer
+  inquirer
     .prompt([
-        {
-            type: 'rawlist',
-            message: 'What would You Like To Do?',
-            name: 'Options',
-            choices: ['Add Department', 'Add Role', 'Add Employee', 'View All Departments', 'View All Roles', 'View all Employees', 'Update Employee Roles', 'QUIT'],
-            validateResponse() {
-            if ('Add Department') {
+      {
+        type: 'list',
+        message: 'What would You Like To Do?',
+        name: 'Options',
+        choices: ['View All Departments', 'Add Department', 'Add Role',  'View All Roles', 'View All Employees','Add Employee', 'Update Employee Roles', 'QUIT'],
+      }])
+    .then((answers) => {
+      const { Options } = answers
 
-             }
-          else if ('Add Role') {
-              
-          }
-          else if ('Add Employee') {
-              console.log('GeneralPublicLicense');
-          }
-          else if ('View All Departments') {
-            console.log('GeneralPublicLicense');
-        }
-        else if ('View All Roles') {
-            console.log('GeneralPublicLicense');
-        }
-        else if ('View All Employees') {
-            console.log('GeneralPublicLicense');
-        }
-        else if ('Update Employee Roles') {
-            console.log('GeneralPublicLicense');
-        }
-        
-          else { ////QUIT////
-              console.log('QUIT');
-          }
-          
-        }
-        
-        
-        },
-       
-    ])};
-    
+      if (Options === "View All Departments") {
+        ViewDepartment();
+      }
+      if (Options === "Add Department") {
+        addDepartment();
+      }
+      if (Options === 'View All Roles') {
+        viewRoles();
+
+      }
+      if (Options === 'Add Role') {
+        addRole();
+
+      }
+      if (Options === 'View All Employees') {
+        viewEmployees();
+
+      }
+      if (Options === 'Add Employee') {
+        addRole();
+
+      }
+      if (Options === 'Update Employee Roles') {
+        addRole();
+
+      }
+      if (Options === 'QUIT') {
+        quitFunction();
+
+      }
       
+    });
+};
 
-  
-  
 
-  startProgram();
+/// Function for viewing department
+function ViewDepartment() {
+  db.query('SELECT * FROM department', function (err, results) {
+    console.log(results);
+  });
+};
+
+///Function to add department START ///////
+addDepartment = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'departmentAdd',
+      message: 'What department would you like to add?',
+      validate: departmentAdd => {
+        if (departmentAdd) {
+          return true;
+        }else {
+          console.log("Please input a department name you would like to add!")
+          return false;
+        }
+        
+      }
+    }
+  ])
+.then(answer => {
+  db.query(`INSERT INTO department(name)
+  VALUES ('${answer.departmentAdd}');` )
+  ViewDepartment();
+  });
+};
+//// Function to add department END //////
+
+/// Function for adding role
+function ViewDepartment() {
+  db.query('SELECT * FROM department', function (err, results) {
+    console.log(results);
+  })
+};
+
+////// Function for view roles/////
+function viewRoles() {
+  db.query('SELECT * FROM roles', function (err, results) {
+    console.log(results);
+  })
+};
+
+////// FUNCTION FOR VIEW ALL EMPLOYEES//////
+function viewEmployees() {
+  db.query('SELECT * FROM employee', function (err, results) {
+    console.log(results);
+  })
+};
+
+////// QUIT FUNCTION
+function quitFunction() {
+  db.query('exit', function (err, results) {
+    console.log('Goodbye!');
+  })
+};
+
+
+
+startProgram();
+
+  // export PATH=${PATH}:/usr/local/mysql/bin* 
+  function map();
